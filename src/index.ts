291ee4c33243b4f2bcd73012696b7df8c2f15e00
raaone7 <ramshyam1982@gmail.com>
2024-03-22 10:21:31 +1100

    some more instructions added

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,24 @@ import { createSimulator } from "./simulator.js";
 const match = new MatchManager({ side1: "P1", side2: "P2", totalSets: 3 });
 const simulator = createSimulator(match);
 
-simulator.playRegularSet(1); // set1: 6-2
+// simulator.playRegularSet(1); // set1: 6-2
 // match.score();
 
-simulator.playSetWithTieBreakerGame(2); // set2: 6-7
+// simulator.playSetWithTieBreakerGame(2); // set2: 6-7
 // match.score();
 
-// match.pointWonBy(1);
-// match.pointWonBy(1);
-// match.pointWonBy(1);
-// match.pointWonBy(2);
-// match.pointWonBy(2);
+match.pointWonBy(1);
+match.pointWonBy(1);
+match.pointWonBy(1);
+match.pointWonBy(2);
+match.pointWonBy(2);
 // match.pointWonBy(2);
 // match.pointWonBy(1);
 
-simulator.playRegularSet(1); // set3: 6-2
+// simulator.playRegularSet(1); // set3: 6-2
 match.score(); // match won by player 1
 // match.printMatchData(); // PRINT rich json
+
+// TODO: please feel free to play the match, and set, any game combination
+// a simulator has been added for convenience to play all the games in the set
+
